e72135cf06f34c8f598dbb383f99544d59cbd1f6
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-21 11:39:40 -0500

    Finished

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JFrame aframe = new JFrame("This is a frame");
+        final int WIDTH = 250;
+        final int HEIGHT = 250;
+        aframe.setSize(WIDTH, HEIGHT);
+        aframe.setVisible(true);
+
     }
 }
